6c66d78921eab33032508c1f325141f3b13a4f04
Le.Sigwal <61907278+le-sigwal@users.noreply.github.com>
2020-04-20 20:31:22 +0200

    Create wisout.cpp

---
+++ wisout.cpp
@@ -0,0 +1,121 @@
+//-------------------------------------------------------------------------
+/* File: wisout.cpp
+ * Description: "WIth SOcket oUT" Project
+ * Authors: Bagad Sigwal
+ * Copyright: 2020 Sigwal.info/github - All Rights Reserved
+ * Last modification: 10/04/2020
+ * Version: 20.04
+ *
+ * Change records:
+ * SWL - 10/04/2020 - creation, added main() for debug
+ *                    added printfa(), printfe(),
+ *                    added send(), select(), recv(), closesocket(),
+ *                      WSAStartup(), gethostbyname(), htons),
+ *                      socket() & connect() for win32                   */
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+//[>DEFINITIONS]
+//-------------------------------------------------------------------------
+//[>STANDARD DEFINITIONS]
+#include <stdio.h>
+#ifdef WIN32
+#  include <winsock2.h>
+#  undef send
+#  undef select
+#  undef recv
+#  undef closesocket
+#  undef WSAStartup
+#  undef gethostbyname
+#  undef htons
+#  undef socket
+#  undef connect
+#  undef WINSOCK_API_LINKAGE
+#  define WINSOCK_API_LINKAGE
+#endif
+//[>EXTERN DEFINITIONS]
+//[>PROJECT DEFINITIONS]
+//[>LOCAL DEFINITIONS]
+#define WISOUT_VERSION "20.04"
+//
+int wstfcn=0, wstfci; // function call level
+//-------------------------------------------------------------------------
+//[<DEFINITIONS]
+//-------------------------------------------------------------------------
+
+void wstprintfa(const char *cfichier, const char *cfonction){
+	for (wstfci=0;wstfci<wstfcn;wstfci++){ printf("  "); } wstfcn++;
+	printf("[>%s.%s]\n", cfichier, cfonction);
+	fflush(stdout); //[dvr]
+}
+
+void wstprintfe(const char *cfichier, const char *cfonction){
+	wstfcn--; for (wstfci=0;wstfci<wstfcn;wstfci++){ printf("  "); }
+	printf("[<%s.%s]\n", cfichier, cfonction);
+	fflush(stdout); //[dvr]
+}
+
+#ifdef WIN32
+extern "C" int send(SOCKET s, const char *buf, int len, int flags){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" int select(int nfds, fd_set *readfds, fd_set *writefds,
+		fd_set *exceptfds, const PTIMEVAL timeout){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" int recv(SOCKET s,char *buf,int len,int flags){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" int closesocket(SOCKET s){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" int WSAStartup(WORD wVersionRequested,LPWSADATA lpWSAData){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" struct hostent *gethostbyname(const char *name){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" u_short htons(u_short hostshort){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" SOCKET socket(int af,int type,int protocol){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+extern "C" int connect(SOCKET s,const struct sockaddr *name,int namelen){
+	wstprintfa(__FILE__, __FUNCTION__);
+	wstprintfe(__FILE__, __FUNCTION__);
+	return 0;
+}
+
+#endif
+
+#ifdef DVR_WISOUT
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+#endif
