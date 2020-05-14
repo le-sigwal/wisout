f32dbc37ab9f125b3496089132b0b627a047fbdb
Le.Sigwal <61907278+le-sigwal@users.noreply.github.com>
2020-05-14 23:20:47 +0200

    added WINAPI

--- wisout.cpp
+++ wisout.cpp
@@ -3,8 +3,8 @@
  * Description: "WIth SOcket oUT" Project
  *                Simple socket reset
  * Authors: Bagad Sigwal
- * Copyright: 2020 Sigwal.info/github - All Rights Reserved
- * Last modification: 12/05/2020
+ * Copyright: 2020 Sigwal.info/github, All Rights Reserved
+ * Last modification: 14/05/2020
  * Version: 20.05
  *
  * Change records:
@@ -13,7 +13,8 @@
  *                    added send(), select(), recv(), closesocket(),
  *                      WSAStartup(), gethostbyname(), htons),
  *                      socket() & connect() for win32
- * SWL - 12/05/2020 - winsock2_wst.h inclusion                           */
+ * SWL - 12/05/2020 - winsock2_wst.h inclusion
+ * SWL - 14/05/2020 - added WINAPI                                       */
 //-------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------
@@ -47,56 +48,57 @@ void wstprintfe(const char *cfichier, const char *cfonction){
 }
 
 #ifdef WIN32
-extern "C" int send(SOCKET s, const char *buf, int len, int flags){
+extern "C" int WINAPI send(SOCKET s, const char *buf, int len, int flags){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
 }
 
-extern "C" int select(int nfds, fd_set *readfds, fd_set *writefds,
+extern "C" int WINAPI select(int nfds, fd_set *readfds, fd_set *writefds,
 		fd_set *exceptfds, const PTIMEVAL timeout){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
 }
 
-extern "C" int recv(SOCKET s,char *buf,int len,int flags){
+extern "C" int WINAPI recv(SOCKET s, char *buf, int len, int flags){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
 }
 
-extern "C" int closesocket(SOCKET s){
+extern "C" int WINAPI closesocket(SOCKET s){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
 }
 
-extern "C" int WSAStartup(WORD wVersionRequested, LPWSADATA lpWSAData){
+extern "C" int WINAPI WSAStartup(WORD wVersionRequested, LPWSADATA lpWSAData){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
 }
 
-extern "C" struct hostent *gethostbyname(const char *name){
+extern "C" struct hostent *WINAPI gethostbyname(const char *name){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return NULL;
 }
 
-extern "C" u_short htons(u_short hostshort){
+extern "C" u_short WINAPI htons(u_short hostshort){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
 }
 
-extern "C" SOCKET socket(int af,int type,int protocol){
+extern "C" SOCKET WINAPI socket(int af, int type, int protocol){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
 }
 
-extern "C" int connect(SOCKET s,const struct sockaddr *name,int namelen){
+extern "C" int WINAPI connect(SOCKET s, const struct sockaddr *name,
+		int namelen){
 	wstprintfa(__FILE__, __FUNCTION__);
 	wstprintfe(__FILE__, __FUNCTION__);
 	return 0;
